91de78ed432bcb7fd8b0629702854cde62190f78
EMMANUELNZYOKI <emmanuelnzyoki777@gmail.com>
2023-07-24 20:36:58 +0300

    added new files

---
+++ js/music-list.js
@@ -0,0 +1,94 @@
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Down below - Roddy",
+    artist: "Roddy Ricch",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Inauma - Bien",
+    artist: "Bien",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "POP SMOKE-element",
+    artist: "pop smoke",
+    img: "music-9",
+    src: "music-9"
+  },
+   {
+    name: "POP SMOKE-dreaming",
+    artist: "pop smoke",
+    img: "music-10",
+    src: "music-10"
+   },
+   {
+    name: "Lil jay-Sex sounds",
+    artist: "Lil jay",
+    img: "music-11",
+    src: "music-11"
+   },
+   {
+    name: "Khaid-jolie",
+    artist: "Khaid",
+    img: "music-12",
+    src: "music-12"
+   },
+   {
+    name: "Boutross-Behavior",
+    artist: "Boutross",
+    img: "music-13",
+    src: "music-13"
+   },
+  {
+    name: "CJ-whoopty",
+    artist: "CJ",
+    img: "music-14",
+    src: "music-14"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
